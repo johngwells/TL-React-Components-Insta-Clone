1c9c110b511106079d929cbddf8b6919c8a9408b
John Wells <molekewl2017@gmail.com>
2020-01-23 10:02:38 -0800

    map through data & display data on webpage

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -4,10 +4,20 @@ import Post from "./Post";
 import "./Posts.css";
 
 // pass the data from App.js down as props then map through the data
-const PostsPage = () => {
+const PostsPage = (props) => {
   return (
     <div className="posts-container-wrapper">
       {/* map through data here */}
+      <h1>Test</h1>
+      {console.log(props)}
+      {props.data.map((p) => {
+        console.log(p)
+        return (
+          <div>
+            <Post post={p}/>
+          </div>
+        );
+      })}
     </div>
   );
 };
